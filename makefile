88a0022a8af19edf4df1ea130829caa369d013b6
Danas Mikelinskas <danas.mikelinskas@gmail.com>
2011-05-17 11:47:29 +0300

    issue fixed: if column name just after 'select' contained keyword in it self, then column name was splitted (reason - there was no matching for DBOBJECT) added handling of: `in' statements (constant list and subquery), comments (one line and multiline)

--- makefile
+++ makefile
@@ -1,13 +1,22 @@
-test:fsqlf
-	cat test_text.sql | ./fsqlf
+CC=gcc
+#to build for windows use : CC=i586-mingw32msvc-gcc
 
-fsqlf:lex.yy.c
-	gcc -o fsqlf lex.yy.c -lfl
-	#strip sqlb
-lex.yy.c: fsqlf.l print_keywords.h settings.h debuging.h global_variables.h
-	flex fsqlf.l
+EXEC=fsqlf
+SRC=fsqlf.l
+INCLUDES=print_keywords.h settings.h debuging.h global_variables.h
+
+
+
+test:fsqlf
+	cat test_text.sql | ./$(EXEC)
+
+$(EXEC):lex.yy.c 
+	$(CC) lex.yy.c -lfl  -o $(EXEC)
+	strip $(EXEC)
+lex.yy.c: $(SRC) $(INCLUDES)
+	flex $(SRC)
 
 clean:
 	rm -f lex.yy.c 
-	rm -f fsqlf
+	rm -f $(EXEC)
 
--- test_text.sql
+++ test_text.sql
@@ -1,16 +1,19 @@
- select baba as g 
+ select asbaba as g 
  , cast( bfbfb as date) +1 as x
+, min(a) over (partition by x,c order by r rows between 1 preceeding and 1 preceeding)
 ,'dfgdfgdfgdfg' as
  c, cast(19.2 as decimal(
 18,2)) , cast(   
  19.2   
   as  
-   decimal(  
+   decimal( /*****sfsfsfsfsfsf//////sfsff/sf/sf/sf/888**********sfsfsfsf
+sfsfsfsfsfsf*******sfsfs*****/
+
  18   ,   
   2     
    )   
   )     
-  ,extract(day from t) as _toto 
+  ,asextract(day from t) as _toto -- komentaras case select from
   from bobo    as gogo   left join baba on 1=1   and 1=1 and 1=1       left outer   join baba  on g=z   cross join gaga 
   where toto and exists 
   (select 1 from t where b=g and 
