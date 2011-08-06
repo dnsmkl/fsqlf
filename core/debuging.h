543cdc47af7faf495203395bbb62c6d9b0697a4e
dnsmkl <danas.mikelinskas@gmail.com>
2011-08-06 22:42:09 +0300

    Move debug settings to debugging.h
    Earlier debuging settings were in settings.h
    It is move because it does not reduce any complexity having it in separate file

--- core/debuging.h
+++ core/debuging.h
@@ -4,6 +4,14 @@
 #include "settings.h"
 
 
+#define DEBUGNONE (0)
+#define DEBUGSTATES (1)
+#define DEBUGMATCHES (2)
+#define DEBUGPARCOUNTS (4)
+
+const int debug_level = DEBUGNONE ;//| DEBUGSTATES | DEBUGMATCHES;
+
+
 // TODO :
 // int ctr[YY_NUM_RULES];
 // #define YY_USER_ACTION ++ctr[yy_act]
--- core/settings.h
+++ core/settings.h
@@ -1,12 +1,7 @@
 #ifndef settings_h
 #define settings_h
 
-#define DEBUGNONE (0)
-#define DEBUGSTATES (1)
-#define DEBUGMATCHES (2)
-#define DEBUGPARCOUNTS (4)
 
-const int debug_level = DEBUGNONE ;//| DEBUGSTATES | DEBUGMATCHES;
 
 
 
