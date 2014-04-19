ce50d09f1ed3f9ff089a5d0c4cdc677c92458637
dnsmkl <danas.mikelinskas@gmail.com>
2014-04-19 09:06:14 +0300

    Change enum name menuControls->EventIds
    To actualy reflect the content (which changed over time I guess)

--- gui/wx_fsqlf.hpp
+++ gui/wx_fsqlf.hpp
@@ -47,7 +47,7 @@ class Notepad : public wxFrame {
     void create_options_text(wxSizer* sizer);
     void create_textarea(wxSizer* parent);
 
-    enum MenuControls{ idSave = 1000, idOpen, idExit, idFormat, idUnformat, idCut, idCopy, idPaste, idSelectAll, idAbout, idMaxLen  };
+    enum EventIds{ idSave = 1000, idOpen, idExit, idFormat, idUnformat, idCut, idCopy, idPaste, idSelectAll, idAbout, idMaxLen  };
 
     DECLARE_EVENT_TABLE()
 };
