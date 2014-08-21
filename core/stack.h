0d0146d0ac81348b18a4f84b6d7e92ced69956a4
danas.mikelinskas@gmail.com <danas.mikelinskas@gmail.com>
2014-08-21 17:59:24 +0300

    Rename stack functions to be 'int' specific

--- core/stack.h
+++ core/stack.h
@@ -5,7 +5,6 @@
 
 #define STATE_STACK_SIZE (100)
 
-typedef int stackitem;
 
 typedef struct
 {
@@ -13,17 +12,17 @@ typedef struct
     // first item => items[0]
     // stack empty => length=0
     size_t length;
-    stackitem items[STATE_STACK_SIZE];
-} stack;
+    int items[STATE_STACK_SIZE];
+} int_stack;
 
 
-void stack_init(stack* stk)
+void int_stack_init(int_stack* stk)
 {
     stk->length = 0;
 }
 
 
-void stack_push(stack* stk, stackitem newitem)
+void int_stack_push(int_stack* stk, int newitem)
 {
     assert(stk->length >= 0 && stk->length < STATE_STACK_SIZE);
     stk->items[stk->length] = newitem;
@@ -31,7 +30,7 @@ void stack_push(stack* stk, stackitem newitem)
 }
 
 
-stackitem stack_pop(stack* stk)
+int int_stack_pop(int_stack* stk)
 {
     stk->length--;
     assert(stk->length >= 0 && stk->length < STATE_STACK_SIZE);
@@ -39,20 +38,20 @@ stackitem stack_pop(stack* stk)
 }
 
 
-stackitem stack_peek(const stack* stk)
+int int_stack_peek(const int_stack* stk)
 {
     assert(stk->length > 0 && stk->length < STATE_STACK_SIZE);
     return stk->items[stk->length-1];
 }
 
 
-int stack_empty(const stack* stk)
+int int_stack_empty(const int_stack* stk)
 {
     return stk->length==0;
 }
 
 
-void stack_dump(stack stk)
+void int_stack_dump(int_stack stk)
 {
     printf("%d:%d %d %d %d\n", stk.length, stk.items[0], stk.items[1], stk.items[2], stk.items[3]);
     fflush(stdout);
@@ -60,37 +59,37 @@ void stack_dump(stack stk)
 
 
 //#include <stdio.h>
-void stack_testing_suite()
+void int_stack_testing_suite()
 {
     //puts("stack_testing_suite:start\n");
-    stack t; // t=test
-    stack_init(&t);
-    assert(stack_empty(&t));
+    int_stack t; // t=test
+    int_stack_init(&t);
+    assert(int_stack_empty(&t));
 
-    stack_push(&t,1);
-    assert(!stack_empty(&t));
-    assert(stack_peek(&t)==1);
-    stack_push(&t,3);
-    assert(stack_peek(&t)==3);
-    stack_push(&t,9);
-    assert(stack_peek(&t)==9);
-    stack_push(&t,41);
-    assert(stack_peek(&t)==41);
+    int_stack_push(&t,1);
+    assert(!int_stack_empty(&t));
+    assert(int_stack_peek(&t)==1);
+    int_stack_push(&t,3);
+    assert(int_stack_peek(&t)==3);
+    int_stack_push(&t,9);
+    assert(int_stack_peek(&t)==9);
+    int_stack_push(&t,41);
+    assert(int_stack_peek(&t)==41);
 
-    assert(stack_pop(&t)==41);
-    assert(stack_peek(&t)==9);
-    assert(stack_pop(&t)==9);
-    assert(stack_peek(&t)==3);
-    assert(stack_pop(&t)==3);
+    assert(int_stack_pop(&t)==41);
+    assert(int_stack_peek(&t)==9);
+    assert(int_stack_pop(&t)==9);
+    assert(int_stack_peek(&t)==3);
+    assert(int_stack_pop(&t)==3);
 
-    stack_push(&t,123);
-    assert(stack_peek(&t)==123);
+    int_stack_push(&t,123);
+    assert(int_stack_peek(&t)==123);
 
-    assert(stack_pop(&t)==123);
-    assert(stack_peek(&t)==1);
-    assert(stack_pop(&t)==1);
+    assert(int_stack_pop(&t)==123);
+    assert(int_stack_peek(&t)==1);
+    assert(int_stack_pop(&t)==1);
 
-    assert(stack_empty(&t));
+    assert(int_stack_empty(&t));
     //puts("stack_testing_suite:success\n");
 }
 
