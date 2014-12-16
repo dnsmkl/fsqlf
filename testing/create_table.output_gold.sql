615db8693cc15aa839854f3bb94d521b0ead3ef6
dnsmkl <danas.mikelinskas@gmail.com>
2014-12-16 21:54:04 +0200

    Add test file for table creation

---
+++ testing/create_table.output_gold.sql
@@ -0,0 +1,6 @@
+
+
+CREATE
+
+TABLE schema_name.table_name ( id int,col2 varchar ( 30 ),col3 non standart keywords char ( 1 ) )
+;
---
+++ testing/create_table.sql
@@ -0,0 +1,1 @@
+create table schema_name.table_name(id int, col2 varchar(30), col3 non standart keywords char(1));
