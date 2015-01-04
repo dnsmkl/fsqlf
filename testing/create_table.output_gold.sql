90e61cc30f0e6133ea486464d7c88f17b3c915c8
dnsmkl <danas.mikelinskas@gmail.com>
2015-01-04 19:24:34 +0200

    Add test for CREATE VOLATILE TALBE (shows bug)

--- testing/create_table.output_gold.sql
+++ testing/create_table.output_gold.sql
@@ -7,3 +7,14 @@ id int
 , col3 non standart keywords char ( 1 )
 )
 ;
+
+
+
+
+CREATE TABLE schema_name.table_name
+(
+id int
+, col2 varchar ( 30 )
+, col3 non standart keywords char ( 1 )
+)
+;
--- testing/create_table.sql
+++ testing/create_table.sql
@@ -1,1 +1,5 @@
 create table schema_name.table_name(id int, col2 varchar(30), col3 non standart keywords char(1));
+
+
+create
+volatile multiset table schema_name.table_name(id int, col2 varchar(30), col3 non standart keywords char(1));
