c65e7f58dd4d49abe4ccad9b4e219209d40271eb
dnsmkl <danas.mikelinskas@gmail.com>
2015-01-13 20:15:35 +0200

    Fix creation of formatting.conf, ensure spacing
    Given setting with long enough name, spacing between name and first number
    dissapeared previously.

--- core/conf_file_create.h
+++ core/conf_file_create.h
@@ -14,24 +14,24 @@ int create_config_file( char* config_file_name ) {
     fputs("# Lines starting with '#' are comments.  Comments and empty lines are ignored\n", config_file);
     fputs("# If there are couple of lines with same setting_name, then only the last one has effect\n\n" , config_file);
 
-    fputs("# space_after -----------------------------------------+\n", config_file);
-    fputs("# tab_after ------------------------------------+      |\n", config_file);
-    fputs("# new_line_after ------------------------+      |      |\n", config_file);
-    fputs("# space_before -------------------+      |      |      |\n", config_file);
-    fputs("# tab_before --------------+      |      |      |      |\n", config_file);
-    fputs("# new_line_before --+      |      |      |      |      |\n", config_file);
-    fputs("#                   |      |      |      |      |      |\n", config_file);
+    fputs("# space_after ----------------------------------------------+\n", config_file);
+    fputs("# tab_after -----------------------------------------+      |\n", config_file);
+    fputs("# new_line_after -----------------------------+      |      |\n", config_file);
+    fputs("# space_before ------------------------+      |      |      |\n", config_file);
+    fputs("# tab_before -------------------+      |      |      |      |\n", config_file);
+    fputs("# new_line_before -------+      |      |      |      |      |\n", config_file);
+    fputs("#                        |      |      |      |      |      |\n", config_file);
     fputs("# setting_name\n", config_file);
 
     #define     QUOTE(name)     #name
     #define T_KW_SETTINGS_MACRO( NAME,nlb,tb,sb,nla,ta,sa,TEXT , ... ) \
-        fprintf(config_file, "%-20s", QUOTE(NAME) ); \
-        fprintf(config_file, "%s", QUOTE(nlb) ); \
-        fprintf(config_file, "%7s",QUOTE(tb) ); \
-        fprintf(config_file, "%7s",QUOTE(sb) ); \
-        fprintf(config_file, "%7s",QUOTE(nla) ); \
-        fprintf(config_file, "%7s",QUOTE(ta) ); \
-        fprintf(config_file, "%7s",QUOTE(sa) ); \
+        fprintf(config_file, "%-24s ", QUOTE(NAME) ); \
+        fprintf(config_file, "%s"  , QUOTE(nlb) ); \
+        fprintf(config_file, " %6s", QUOTE(tb) ); \
+        fprintf(config_file, " %6s", QUOTE(sb) ); \
+        fprintf(config_file, " %6s", QUOTE(nla) ); \
+        fprintf(config_file, " %6s", QUOTE(ta) ); \
+        fprintf(config_file, " %6s", QUOTE(sa) ); \
         fprintf(config_file, "\n") ;
     #include "t_kw_settings_list.def"
     #undef T_KW_SETTINGS_MACRO
