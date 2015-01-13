99685d5d9b07f6bdeb72f6cc39932505b8d5506b
dnsmkl <danas.mikelinskas@gmail.com>
2015-01-13 20:44:19 +0200

    Stylistic changes (comments, spacing, less code lines)

--- core/conf_file_create.h
+++ core/conf_file_create.h
@@ -1,15 +1,22 @@
 #ifndef create_conf_file_h
 #define create_conf_file_h
 
-int create_config_file( char* config_file_name ) {
+
+// Create formatting configuration file with default content.
+// Return values:
+//  0 on success
+//  1 on failure to open
+//  2 on failure to close
+int create_config_file(char* config_file_name)
+{
     FILE * config_file;
     config_file = fopen(config_file_name, "w");
 
-    if ( !(config_file) ){ // Create config file
+    if (!config_file) {
         fprintf(stderr, "Failed to create '%s' file!\n", config_file_name);
         return 1;
     }
-        // Write default configuration to "formatting.conf" file:
+
     fputs("# This file contains formatting (spacing) settings wich can be used to override the default formatting style used by FSQLF\n", config_file);
     fputs("# Lines starting with '#' are comments.  Comments and empty lines are ignored\n", config_file);
     fputs("# If there are couple of lines with same setting_name, then only the last one has effect\n\n" , config_file);
@@ -23,19 +30,15 @@ int create_config_file( char* config_file_name ) {
     fputs("#                        |      |      |      |      |      |\n", config_file);
     fputs("# setting_name\n", config_file);
 
-    #define     QUOTE(name)     #name
-    #define T_KW_SETTINGS_MACRO( NAME,nlb,tb,sb,nla,ta,sa,TEXT , ... ) \
-        fprintf(config_file, "%-24s ", QUOTE(NAME) ); \
-        fprintf(config_file, "%s"  , QUOTE(nlb) ); \
-        fprintf(config_file, " %6s", QUOTE(tb) ); \
-        fprintf(config_file, " %6s", QUOTE(sb) ); \
-        fprintf(config_file, " %6s", QUOTE(nla) ); \
-        fprintf(config_file, " %6s", QUOTE(ta) ); \
-        fprintf(config_file, " %6s", QUOTE(sa) ); \
-        fprintf(config_file, "\n") ;
+    // Define macro to print one line containing config of single keyword
+    // and run it (via #include) for each keyword.
+    #define QQ(name) #name
+    #define T_KW_SETTINGS_MACRO(NAME, nlb, tb, sb, nla, ta, sa, ... )   \
+        fprintf(config_file, "%-24s %s %6s %6s %6s %6s %6s\n",          \
+            QQ(NAME), QQ(nlb), QQ(tb), QQ(sb), QQ(nla), QQ(ta), QQ(sa));
     #include "t_kw_settings_list.def"
     #undef T_KW_SETTINGS_MACRO
-
+    #undef QQ
 
     fputs("\n\n", config_file);
     fputs("# Some explanations regarding names (shortenings) used above:\n", config_file);
@@ -48,9 +51,9 @@ int create_config_file( char* config_file_name ) {
     fputs("# - kw = keyword (though for some reason it's used also for puntuantion character..)\n", config_file);
     fputs("\n\n", config_file);
 
-    if (fclose(config_file) == 0) // Write and close config file
+    if (fclose(config_file) == 0) {
         return 0;
-    else {
+    } else {
         fprintf(stderr, "Failed to close '%s' file!\n", config_file_name);
         return 2;
     }
