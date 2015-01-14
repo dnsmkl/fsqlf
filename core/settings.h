3d1f92dd3714298ea8bf76c09a3ecea92dcfe9d5
dnsmkl <danas.mikelinskas@gmail.com>
2015-01-14 21:34:29 +0200

    Move functions used only for printing
    setting.h contained lot of functions that were used only for printing,
    they do not seem to belong here.
    e.g. calculate_spacing, stocase

--- core/settings.h
+++ core/settings.h
@@ -9,12 +9,6 @@
 const char * tab_string = "    "; //indentation is done with 4 spaces
 
 
-inline int max(int a, int b)
-{
-    return a > b ? a : b;
-}
-
-
 // struture to store text keyword text  space,tab,newline, function to execute  before/after printig the keyword
 typedef struct
 {
@@ -41,61 +35,6 @@ typedef struct
 
 
 void debug_p(); // TODO : make separate .c and .h files
-
-
-int max_or_current(int prev_count, int curr_count, char use_only_curr_ind)
-{
-    if (use_only_curr_ind) {
-        return curr_count;
-    }
-    else{
-        return max(prev_count, curr_count);
-    }
-}
-
-
-spacing_counts calculate_spacing(
-    spacing_counts afterspacing_of_prev,
-    unsigned short int isword_of_prev,
-    spacing_counts beforespacing_of_current,
-    unsigned short int isword_of_current,
-    int global_indent_level)
-{
-    /* Combine previous and current words adjacent spacings.
-        Combination is done by using MAX or just using current words settings.
-        Decision is made so:
-            There are 3 types of spacings and have ranks: space-1, indentation-2, new line-3.
-            if current word's has active spacing setting (not 0),
-            then all spacings of lower ranks, must be used from current word
-        Rationale for such logic:
-            if word has in its settings, new-line=1 and indent/spaces=0,
-            then there is expectation of keyword being at the start of the line (not counting global indent level because of subselect)
-    */
-    spacing_counts r; // result to be built
-
-
-    r.new_line = max(afterspacing_of_prev.new_line, beforespacing_of_current.new_line);
-
-    r.indent = max_or_current(
-            afterspacing_of_prev.indent
-            , beforespacing_of_current.indent
-            , beforespacing_of_current.new_line>1);
-    r.indent += r.new_line ? global_indent_level : 0;
-
-    r.space = max_or_current(
-            afterspacing_of_prev.space
-            , beforespacing_of_current.space
-            , beforespacing_of_current.new_line || beforespacing_of_current.indent);
-
-
-    // adjacent words MUST have some spacing
-    if (!r.new_line && !r.indent && !r.space &&
-        isword_of_prev && isword_of_current) {
-        r.space = 1;
-    }
-
-    return r;
-}
 
 
 // Initialize t_kw_settings variables.
@@ -152,37 +91,6 @@ void init_all_settings()
     } while (0);
     #include "t_kw_settings_list.def"
     #undef T_KW_SETTINGS_MACRO
-}
-
-
-#define MAX_KEYWORD_SIZE (50)
-char *stocase(char* s_text, unsigned short int s_case)
-{
-    static char formatted_result[MAX_KEYWORD_SIZE];
-    int i;
-
-    switch (s_case) {
-        case CASE_lower:
-            for (i = 0; i<strlen(s_text); i++) {
-                formatted_result[i] = tolower(s_text[i]);
-            }
-            break;
-        case CASE_UPPER:
-            for (i = 0; i<strlen(s_text); i++) {
-                formatted_result[i] = toupper(s_text[i]);
-            }
-            break;
-        case CASE_Initcap:
-            formatted_result[0] = toupper(s_text[0]);
-            for (i = 1; i<strlen(s_text); i++) {
-                formatted_result[i] = tolower(s_text[i]);
-            }
-            break;
-        case CASE_none:
-            return s_text;
-    }
-    formatted_result[strlen(s_text)] = '\0';
-    return formatted_result;
 }
 
 
