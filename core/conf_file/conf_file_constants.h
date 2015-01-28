f4edc704731ad1a7b92504e9ae2c1907fe16ff94
dnsmkl <danas.mikelinskas@gmail.com>
2015-01-28 23:05:59 +0200

    Make conf file constants paramtrizable during compile

--- core/conf_file/conf_file_constants.h
+++ core/conf_file/conf_file_constants.h
@@ -2,8 +2,13 @@
 #define CONF_FILE_CONSTANTS_H
 
 
+#ifndef FSQLF_CONFFILE_NAME
 #define FSQLF_CONFFILE_NAME "formatting.conf"
+#endif
+
+#ifndef FSQLF_CONFFILE_LINELENGTH
 #define FSQLF_CONFFILE_LINELENGTH (100)
+#endif
 
 
 #endif
