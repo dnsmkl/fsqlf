4afefcbc04cb921be0c64f1289957ed8c9eb5ad6
dnsmkl <danas.mikelinskas@gmail.com>
2015-02-11 07:13:21 +0200

    Remove workaround debug function declaration
    Instead of it use include in more appropriate place

--- core/kw/kwall_init.h
+++ core/kw/kwall_init.h
@@ -5,9 +5,6 @@
 #include "kw.h"
 
 
-void debug_p(); // TODO : make separate .c and .h files
-
-
 void set_case(unsigned short int keyword_case);
 void set_text_original(unsigned short int ind_original);
 void init_all_settings(struct kw_conf * (*kw)(const char *));
