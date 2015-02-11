d062f41ea9da0efe893c2dc48a50097e22fcd331
dnsmkl <danas.mikelinskas@gmail.com>
2015-02-11 06:57:12 +0200

    Remove redundant typedef
    Seems reason for strange behaviour was that include was missing.

--- core/cli.h
+++ core/cli.h
@@ -2,11 +2,8 @@
 #define CLI_H
 
 
+#include "kw/kw.h"
 
-// Without following typedef, error is issued:
-// core/cli.h:12:53: warning: 'struct kw_conf' declared inside parameter list [enabled by default]
-// GCC bug?
-typedef struct kw_conf* some_random_name_works_as_workaround;
 
 void read_cli_options(int argc, char **argv,
 	struct kw_conf * (*kw)(const char *));
