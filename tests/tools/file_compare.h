dc34d791c7bd2cb4f714985e5005f9c8ca602de6
dnsmkl <danas.mikelinskas@gmail.com>
2016-02-28 22:45:49 +0200

    Add file_compare()
    Redoing testing part, to make it less dependant
    on linux tooling.

---
+++ tests/tools/file_compare.h
@@ -0,0 +1,17 @@
+#ifndef FILE_COMPARE_H
+#define FILE_COMPARE_H
+
+
+enum fcomp_result
+{
+    FCOMP_MATCH = 0,
+    FCOMP_DIFFER = 1,
+    FCOMP_ERROR = 2
+};
+
+
+// Char-bychar comparison of two files.
+enum fcomp_result file_compare(char *path_left, char *path_right);
+
+
+#endif
